cd3a2481c12f496e678138be12725483a09e889e
Carlos Azofeifa <carlos.azofeifa@snowflake.com>
2026-01-28 11:00:10 -0600

    Update snowconvert-ai-dev cask to v2.4.0-rc.22 (#49)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- Casks/snowconvert-ai-dev.rb
+++ Casks/snowconvert-ai-dev.rb
@@ -2,14 +2,14 @@ cask "snowconvert-ai-dev" do
   name "snowflake-scai-cli"
   desc "AI-powered CLI tool for automated code migration to Snowflake (Development Version)"
   homepage "https://docs.snowflake.com/en/migrations/snowconvert-docs/overview"
-  version "2.4.0-rc.21"
+  version "2.4.0-rc.22"
 
   arch_suffix = Hardware::CPU.intel? ? "x64" : "arm64"
 
   if Hardware::CPU.intel?
-    sha256 "69eaaa260b42adaeb91ccffa5397ba349d8f660431a6a49805808ccf1ea6ec1d"
+    sha256 "dce81c8643a8d9be61a98c764d2e3b164839ba6d82d36fda2739d6df4d0a9ee1"
   else
-    sha256 "03949a0eb166affbba35f10d50037bf82a58f2e642fefbdb57b3ec79cc63f43e"
+    sha256 "20e9916b992e3810760f9a3cbde1693d5285f8fac33155266ecf0326d8459934"
   end
 
   url "https://snowconvert.snowflake.com/storage/darwin_#{arch_suffix}/dev/cli/snowflake-scai-cli-#{version}-darwin-#{arch_suffix}.pkg"
