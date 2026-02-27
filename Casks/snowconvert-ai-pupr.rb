34a90612708e38573f740404a79a116d8e16630a
sfc-gh-migrations-sa <sfc-gh-migrations-sa@snowflake.com>
2026-02-27 23:30:32 +0000

    Update snowconvert-ai-pupr cask to v2.8.0-PuPr.31
    Automated cask update for snowconvert-ai-pupr
    Version: 2.8.0-PuPr.21 -> 2.8.0-PuPr.31
    Auto-generated by Azure DevOps Release Pipeline

--- Casks/snowconvert-ai-pupr.rb
+++ Casks/snowconvert-ai-pupr.rb
@@ -2,14 +2,14 @@ cask "snowconvert-ai-pupr" do
   name "snowflake-scai-cli"
   desc "AI-powered CLI tool for automated code migration to Snowflake (Public Preview)"
   homepage "https://docs.snowflake.com/en/migrations/snowconvert-docs/overview"
-  version "2.8.0-PuPr.21"
+  version "2.8.0-PuPr.31"
 
   arch_suffix = Hardware::CPU.intel? ? "x64" : "arm64"
 
   if Hardware::CPU.intel?
-    sha256 "8c7afee3506ca5167e64957e8c3b3c7426c6ba4eba06a57e690884d6b4f496f3"
+    sha256 "4ddbebd60cfc943c508b1ac68cbac46d9337c81777fa7f45e56c83e7906e6c55"
   else
-    sha256 "2d72843e26e34965ae37b282f5dcb4d60788dd32eeb4df89e7aeaa27faf247a7"
+    sha256 "70b21488014d2e60c85291fdb919bbfe584bfd48bbd496407d18e41677b31f74"
   end
 
   url "https://snowconvert.snowflake.com/storage/darwin_#{arch_suffix}/beta/cli/snowflake-scai-cli-#{version}-darwin-#{arch_suffix}.pkg"
