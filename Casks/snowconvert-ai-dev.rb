bef31b63cde102e72b0532b8d46937b447976620
sfc-gh-migrations-sa <sfc-gh-migrations-sa@snowflake.com>
2026-03-12 14:59:37 +0000

    Update snowconvert-ai-dev cask to v2.15.0-rc.144
    Automated cask update for snowconvert-ai-dev
    Version: 2.15.0-rc.143 -> 2.15.0-rc.144
    Auto-generated by Azure DevOps Release Pipeline

--- Casks/snowconvert-ai-dev.rb
+++ Casks/snowconvert-ai-dev.rb
@@ -2,14 +2,14 @@ cask "snowconvert-ai-dev" do
   name "snowflake-scai-cli"
   desc "AI-powered CLI tool for automated code migration to Snowflake (Development Version)"
   homepage "https://docs.snowflake.com/en/migrations/snowconvert-docs/overview"
-  version "2.15.0-rc.143"
+  version "2.15.0-rc.144"
 
   arch_suffix = Hardware::CPU.intel? ? "x64" : "arm64"
 
   if Hardware::CPU.intel?
-    sha256 "197305f90ad0e30c181a290f6fd8c14d08a48ae376e776e1ab3f2752f1ed08fd"
+    sha256 "134b4011413fa130ecdf444f7196b07c35cdc2543af27e2d58be4fe6ffac7c1f"
   else
-    sha256 "261ff726676ec54552eb944caff6f3a16d9b0185f8468ac417cfc154dd1b8420"
+    sha256 "95cf463c0001958414c3177fcc2c53b33b97ebaa909ebb6a4f0ed50d9de19b25"
   end
 
   url "https://snowconvert.snowflake.com/storage/darwin_#{arch_suffix}/dev/cli/snowflake-scai-cli-#{version}-darwin-#{arch_suffix}.pkg"
